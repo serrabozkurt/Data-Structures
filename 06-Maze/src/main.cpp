23fff68eb4007f4d1173075be7f90cd2a24b7065
Tolga Ovatman <tolga.ovatman@icrontech.com>
2020-11-30 14:52:32 +0300

    shrunk maze main code

--- 06-Maze/src/main.cpp
+++ 06-Maze/src/main.cpp
@@ -21,81 +21,38 @@ int main()
 	Position entrance = {0, 1, 0, 0, 0, 0, 0};
 	Position exit = {7, 1, 0, 0, 0, 0, 0};
 	Position p = entrance;
+	Position past;
 	p.camefrom = LEFT;
-	printmaze(maze);
+	print_maze(maze);
 	bool goback = false;
-
-	while (p.x != exit.x || p.y != exit.y)
+	bool moved = true;
+	
+	while (!reached_exit(p, exit))
 	{
 		maze[p.y][p.x] = 'o';
-		printmaze(maze);
+		print_maze(maze);
 		getchar();
-		//first find in how many directions we can move
-		if (!goback)
-		{ //if not calculated before
-			p.right = 0;
-			p.left = 0;
-			p.down = 0;
-			p.up = 0;
-			if (p.x < 7 && maze[p.y][p.x + 1] != 'x')
-				p.right = 1; //right
-			if (p.x > 0 && maze[p.y][p.x - 1] != 'x')
-				p.left = 1; //left
-			if (p.y < 7 && maze[p.y + 1][p.x] != 'x')
-				p.down = 1; //down
-			if (p.y > 0 && maze[p.y - 1][p.x] != 'x')
-				p.up = 1; //up
+		
+		if (!goback){ 
+			reset_possible_directions(p);
+			check_possible_move(p, maze);
 		}
-		else
-			goback = false;
-		//here, one of the possible moves is selected
-		bool moved = true;
-		Position past = p;
-		if (p.down && p.camefrom != DOWN)
-		{
-			p.y++;
-			p.camefrom = UP;
-			past.down = 0;
+		else goback = false;
+
+		past = p;
+		moved = move_to(p, past);
+
+		if (!reached_exit(p, exit) && made_choice(past))
+			s.push(past);
+			
+		if (!reached_exit(p, exit) && !moved && !s.isempty()){
+			p = s.pop();
+			goback = true;
 		}
-		else if (p.up && p.camefrom != UP)
-		{
-			p.y--;
-			p.camefrom = DOWN;
-			past.up = 0;
-		}
-		else if (p.left && p.camefrom != LEFT)
-		{
-			p.x--;
-			p.camefrom = RIGHT;
-			past.left = 0;
-		}
-		else if (p.right && p.camefrom != RIGHT)
-		{
-			p.x++;
-			p.camefrom = LEFT;
-			past.right = 0;
-		}
-		else
-			moved = false; //one direction (the minimum) is open, but this is the direction we came from
-		if (p.x != exit.x || p.y != exit.y)
-		{
-			if ((past.down + past.up + past.right + past.left) > 1)
-			{
-				//there is more than one choice, push onto stack and //continue in that chosen direction. Let the choices  //you have not selected remain marked on the stack.
-				s.push(past);
-			}
-			if (!moved)
-			{ // has to go back
-				if (!s.isempty())
-				{
-					p = s.pop();
-					goback = true;
-				}
-			}
-		}
-	} //end of while
+	} 
+
 	maze[p.y][p.x] = 'o';
-	printmaze(maze);
+	print_maze(maze);
 	cout << "PATH found" << endl;
 	s.close();
 
