76547f02afb955ca53dfa0ab6d532c7c3a84e290
Tolga Ovatman <tolga.ovatman@icrontech.com>
2020-12-25 15:58:48 +0300

    recursion week

---
+++ 08-Recursion/src/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdlib.h>
+
+#include "tests.h"
+
+using namespace std;
+
+int main()
+{
+	
+	//test_factorial(-1);
+	//test_binary_sum(10);
+	//test_permutations(5);
+	//test_n_queens(4);
+	//test_hanoi(3);
+	//test_recursive_maze();
+
+	getchar();
+	return EXIT_SUCCESS; 
+}
+
+
+
+
